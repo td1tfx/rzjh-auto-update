d2fccaf4ccd0d5f77bcb10a3eb839109864fe481
tfx <td1tfx@163.com>
2019-12-16 21:34:48 +0900

    added version check

--- update.py
+++ update.py
@@ -1,48 +1,143 @@
 import sys
 import requests
 import os
+import urllib
+import json
+import zipfile
+import argparse
+import time
+import threading
+import shutil
+import _pickle as cPickle
+from collections import namedtuple
+from multiprocessing.pool import ThreadPool
+from urllib.parse import urlsplit
 
+# default parameters
+defaults = dict(
+    thread_count=10,
+    buffer_size=500 * 1024,
+    block_size=1000 * 1024)
 
-#
-requests.packages.urllib3.disable_warnings()
+# global lock
+lock = threading.Lock()
 
-def downloadfile(url, file_path):
-    r1=requests.get(url, stream=True, verify=False)
-    total_size=int(r1.hearders["content-length"])
-    if(os.path.exists(file_path)):
-        temp_size=os.path.getsize(file_path)
+def get_file_info(url):  # 发送请求读取要下载的文件大小
+    class HeadRequest(urllib.request.Request):
+        def get_method(self):
+            return "HEAD"
+    res = urllib.request.urlopen(HeadRequest(url))
+    res.read()
+    headers = dict(res.headers)
+    size = int(headers.get('content-length', 0))
+    lastmodified = headers.get('last-modified', '')
+    name = None
+    if 'content-disposition'in headers:
+        name = headers['content-disposition'].split('filename=')[1]
+        if name[0] == '"' or name[0] == "'":
+            name = name[1:-1]
     else:
-        temp_size=0;
-    print(temp_size, total_size)
-    # 断点续传
-    hearders={'Range':'bytes=%d-'%temp_size}
-    r=requests.get(url,stream=True,verify=False,headers=headers)
+        name = os.path.basename(urlsplit(url)[2])
+    return FileInfo(url, name, size, lastmodified)
 
-    with open(file_path, 'ab+') as f:
-        for chunk in r.iter_content(chunk_size=1024):
-            if chunk:
-                temp_size += len(chunk)
-                f.write(chunk)
-                f.flush()
-                done = int(50 * temp_size / total_size)
-                sys.stdout.write("\r[%s%s]%d%%" % ('*' * done, ' ' * (50 - done), 100 * temp_size / total_size))
-                sys.stdout.flush()
-    print()
+FileInfo = namedtuple('FileInfo', 'url name size lastmodified')
 
+def download(url, output,  # 下载
+             thread_count=defaults['thread_count'],
+             buffer_size=defaults['buffer_size'],
+             block_size=defaults['block_size']):
+    file_info = get_file_info(url)
+    if output is None:
+        output = file_info.name
+    workpath = '%s.ing' % output
+    infopath = '%s.inf' % output
+    blocks = []
+    if os.path.exists(infopath):
+        _x, blocks = read_data(infopath)
+        if (_x.url != url or
+                _x.name != file_info.name or
+                _x.lastmodified != file_info.lastmodified):
+            blocks = []
+    if len(blocks) == 0:
+        if block_size > file_info.size:
+            blocks = [[0, 0, file_info.size]]
+        else:
+            block_count, remain = divmod(file_info.size, block_size)
+            blocks = [[i * block_size, i * block_size,
+                       (i + 1) * block_size - 1] for i in range(block_count)]
+            blocks[-1][-1] += remain
+        with open(workpath, 'wb') as fobj:
+            fobj.write('')
+    print('Downloading %s' % url)
+    threading.Thread(target=_monitor, args=(
+        infopath, file_info, blocks)).start()
+    with open(workpath, 'rb+') as fobj:
+        args = [(url, blocks, fobj, buffer_size)
+                for i in range(len(blocks)) if blocks[1] < blocks[2]]
+        if thread_count > len(args):
+            thread_count = len(args)
+        pool = ThreadPool(thread_count)
+        pool.map(_worker, args)
+        pool.close()
+        pool.join()
+    if os.path.exists(output):
+        os.remove(output)
+    os.rename(workpath, output)
+    if os.path.exists(infopath):
+        os.remove(infopath)
+    assert all([block[1] >= block[2] for block in blocks]) is True
 
-def getlocalversion(versionpath):  # 读取本地版本信息
-    f = open(versionpath, 'rb')  # 打开路径下的json文件，‘rb’表示文件可读
+def _worker(url, block, fobj, buffer_size):  # 发送请求，确认后开始下载
+    req = urllib.request.Request(url)
+    req.headers['Range'] = 'bytes=%s-%s' % (block[1], block[2])
+    res = urllib.request.urlopen(req)
+    while 1:
+        chunk = res.read(buffer_size)
+        if not chunk:
+            break
+        with lock:
+            fobj.seek(block[1])
+            fobj.write(chunk)
+            block[1] += len(chunk)
+
+def progress(percent, width=50):  # 显示下载进度
+    print("%s %d%%\r" % (('%%-%ds' % width) % (width * percent / 100 * '>>'), percent)),
+    if percent >= 100:
+        print
+        sys.stdout.flush()
+
+def write_data(filepath, data):
+    with open(filepath, 'wb') as output:
+        cPickle.dump(data, output)
+
+def _monitor(infopath, file_info, blocks):
+    while 1:
+        with lock:
+            percent = sum([block[1] - block[0]
+                           for block in blocks]) * 100 / file_info.size
+            progress(percent)
+            if percent >= 100:
+                break
+            write_data(infopath, (file_info, blocks))
+        time.sleep(2)
+
+def read_data(filepath):
+    with open(filepath, 'rb') as output:
+        return cPickle.load(output)
+
+def getlocalversion(version_path):  # 读取本地版本信息
+    f = open(version_path, 'rb')  # 打开路径下的json文件，‘rb’表示文件可读
     data = f.read()
-    print data
+    print(data)
     j = json.loads(data)
     version = j["version"]  # 读取key"version"对应的value值
-    print version
+    print(version)
     return version
 
-def geturl1(pack_path):  # 发送第一条请求将版本信息上传到服务器
-    url = pack_path + getlocalversion(versionpath)   #将版本号写在http上
-    req = urllib2.Request(url)
-    res_data = urllib2.urlopen(req)
+def geturl1(version_path):  # 发送第一条请求将版本信息上传到服务器
+    #version_path="/version.json"
+    url = getlocalversion(version_path)   #将版本号写在http上
+    res_data = urllib.request.urlopen(url)
     res = res_data.read()
     return res  # 服务器返回信息作为返回值
 
@@ -78,20 +173,54 @@ def newrename(pathT, j, zipfile_name):  # 将解压后的压缩包更名到  包
     os.rename(zipfile_name + "_files", b.encode("utf-8"))
     return b
 
-def reversion(jsonpath):  # 将获取的新版本号替换进老版本信息中
-    f = open(jsonpath, 'r+')
+def reversion(version_path):  # 将获取的新版本号替换进老版本信息中
+    f = open(version_path, 'r+')
     data = f.read()
-    print data
+    print(data)
     j = json.loads(data)
-    j["version"] = getserversion()
-    with open(jsonpath, 'wb') as f:
+    res = geturl1(version_path) 
+    j["version"] = getserversion(res)
+    with open(version_path, 'wb') as f:
         f.write(json.dumps(j))  # 写进json
 
-if __name__=='__main__':
-    link=r'http://130.34.192.71'
-    UUID=r'update.zip'
-    pack_path="rzjh/my.zip"
-    ver_path="rzjh/version.json"
-    version=getlocalversion(ver_path)
-    url=os.path.join(link,UUID)
-    downloadfile(url,path)
+
+def rename(pathT,version_path):  # 更改老版本文件名
+    os.listdir(pathT)
+    b = 'willdele'
+    os.rename('filename' + getlocalversion(version_path), b.encode("utf-8"))
+
+def delold():  # 删除更名后的老版本
+    shutil.rmtree("将要删除的文件夹路径和文件夹名willdele")
+# 参考文献：https://www.cnblogs.com/FengZiQ/p/8532141.html
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='多线程文件下载器.')
+    parser.add_argument('url', type=str, help='下载连接')
+    parser.add_argument('-o', type=str, default=None, dest="output", help='输出文件')
+    parser.add_argument('-t', type=int, default=defaults['thread_count'], dest="thread_count", help='下载的线程数量')
+    parser.add_argument('-b', type=int, default=defaults['buffer_size'], dest="buffer_size", help='缓存大小')
+    parser.add_argument('-s', type=int, default=defaults['block_size'], dest="block_size", help='字区大小')
+    argv = sys.argv[1:]
+    if len(argv) == 0:
+        argv = argv = ['http:XXXXX']
+    args = parser.parse_args(argv)
+    start_time = time.time()
+    pathT = 'rzjh'
+    version_path = "version.json"
+    res = geturl1(version_path)
+    yn = getYN(res)
+    new_version = getserversion(res)
+    zipfile_name = "rzjh" + new_version + ".zip"
+    if yn == 1:
+        print("准备更新，请稍后》》")
+        rename(pathT,version_path)
+        download(args.url, args.output, args.thread_count,
+                 args.buffer_size, args.block_size)
+        reversion(version_path)
+        delold()
+        un_zip(zipfile_name)
+        delzip(pathT, zipfile_name)
+        newrename(pathT, new_version, zipfile_name)
+    else:
+        print ("已经是最新版本《《")
+        exit()
