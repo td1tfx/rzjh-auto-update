8df3c4a4aa6b178db673d7a883b23168d47bd904
td1tfx <td1tfx@163.com>
2019-12-20 19:45:15 +0900

    added thread, auto run rzjh.exe

--- update.py
+++ update.py
@@ -4,23 +4,9 @@ import os
 import urllib
 import json
 import zipfile
-import argparse
 import time
-import threading
 import shutil
-import _pickle as cPickle
-from collections import namedtuple
-from multiprocessing.pool import ThreadPool
-from urllib.parse import urlsplit
-
-# default parameters
-defaults = dict(
-    thread_count=10,
-    buffer_size=500 * 1024,
-    block_size=1000 * 1024)
-
-# global lock
-lock = threading.Lock()
+from threading import Thread
 
 def new_download(url, file_path):
     # 第一次请求是为了得到文件总大小
@@ -54,110 +40,6 @@ def new_download(url, file_path):
                 sys.stdout.write("\r[%s%s] %d%%" % ('█' * done, ' ' * (50 - done), 100 * temp_size / total_size))
                 sys.stdout.flush()
     print()  # 避免上面\r 回车符
-
-
-def get_file_info(url):  # 发送请求读取要下载的文件大小
-    class HeadRequest(urllib.request.Request):
-        def get_method(self):
-            return "HEAD"
-    res = urllib.request.urlopen(HeadRequest(url))
-    res.read()
-    headers = dict(res.headers)
-    size = int(headers.get('Content-Length', 0))
-    lastmodified = headers.get('last-modified', '')
-    name = None
-    if 'content-disposition'in headers:
-        name = headers['content-disposition'].split('filename=')[1]
-        if name[0] == '"' or name[0] == "'":
-            name = name[1:-1]
-    else:
-        name = os.path.basename(urlsplit(url)[2])
-    return FileInfo(url, name, size, lastmodified)
-
-FileInfo = namedtuple('FileInfo', 'url name size lastmodified')
-
-def download(url, output,  # 下载
-             thread_count=defaults['thread_count'],
-             buffer_size=defaults['buffer_size'],
-             block_size=defaults['block_size']):
-    file_info = get_file_info(url)
-    if output is None:
-        output = file_info.name
-    workpath = '%s.ing' % output
-    infopath = '%s.inf' % output
-    blocks = []
-    if os.path.exists(infopath):
-        _x, blocks = read_data(infopath)
-        if (_x.url != url or
-                _x.name != file_info.name or
-                _x.lastmodified != file_info.lastmodified):
-            blocks = []
-    if len(blocks) == 0:
-        if block_size > file_info.size:
-            blocks = [[0, 0, file_info.size]]
-        else:
-            block_count, remain = divmod(file_info.size, block_size)
-            blocks = [[i * block_size, i * block_size,
-                       (i + 1) * block_size - 1] for i in range(block_count)]
-            blocks[-1][-1] += remain
-        with open(workpath, 'wb') as fobj:
-            fobj.write(''.encode(encoding='UTF8'))
-    print('Downloading %s' % url)
-    threading.Thread(target=_monitor, args=(
-        infopath, file_info, blocks)).start()
-    with open(workpath, 'rb+') as fobj:
-        args = [(url, blocks, fobj, buffer_size)
-                for i in range(len(blocks)) if blocks[i-1] < blocks[i]]
-        if thread_count > len(args):
-            thread_count = len(args)
-        pool = ThreadPool(thread_count)
-        pool.map(_worker, args)
-        pool.close()
-        pool.join()
-    if os.path.exists(output):
-        os.remove(output)
-    os.rename(workpath, output)
-    if os.path.exists(infopath):
-        os.remove(infopath)
-    assert all([block[1] >= block[2] for block in blocks]) is True
-
-def _worker(url, block, fobj, buffer_size):  # 发送请求，确认后开始下载
-    req = urllib.request.Request(url)
-    req.headers['Range'] = 'bytes=%s-%s' % (block[1], block[2])
-    res = urllib.request.urlopen(req)
-    while 1:
-        chunk = res.read(buffer_size)
-        if not chunk:
-            break
-        with lock:
-            fobj.seek(block[1])
-            fobj.write(chunk)
-            block[1] += len(chunk)
-
-def progress(percent, width=50):  # 显示下载进度
-    print("%s %d%%\r" % (('%%-%ds' % width) % (width * percent / 100 ), percent)),
-    if percent >= 100:
-        print
-        sys.stdout.flush()
-
-def write_data(filepath, data):
-    with open(filepath, 'wb') as output:
-        cPickle.dump(data, output)
-
-def _monitor(infopath, file_info, blocks):
-    while 1:
-        with lock:
-            percent = sum([block[1] - block[0]
-                           for block in blocks]) * 100 / file_info.size
-            progress(percent)
-            if percent >= 100:
-                break
-            write_data(infopath, (file_info, blocks))
-        time.sleep(2)
-
-def read_data(filepath):
-    with open(filepath, 'rb') as output:
-        return cPickle.load(output)
 
 def getlocalversion(version_path):  # 读取本地版本信息
     f = open(version_path, 'rb')  # 打开路径下的json文件，‘rb’表示文件可读
@@ -263,8 +145,10 @@ def movetree(path_s, path_d):
 def excuteexe():
     main_exe = "In_stories.exe"
     if os.path.exists(main_exe):
-        rv = os.system(main_exe)
-        print(rv)
+        thread = Thread()
+        thread.run = lambda: os.system(main_exe)
+        thread.start()
+        print("run ", main_exe)
 
 def update():
     start_time = time.time()
@@ -286,8 +170,9 @@ def update():
         delzip(pathT, zipfile_name)
         new_name = newrename(pathT, new_version, zipfile_name)
         print(new_name)
-        movetree(new_name, sys.path[0])
+        movetree(new_name, ".\\")
         excuteexe()
+        exit()
     else:
         print ("已经是最新版本《《")
         excuteexe()
